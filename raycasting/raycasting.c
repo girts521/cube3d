8b9f1d3665a20570a00de4f6b1297c5e03c251ec
Alex <email@test.com>
2026-01-15 22:30:37 +0100

    raycasting formatting

--- raycasting/raycasting.c
+++ raycasting/raycasting.c
@@ -1,207 +1,147 @@
 #include "../cub3d.h"
 #include <math.h>
 
-void	put_pixel(mlx_image_t* image, uint32_t x, uint32_t y, uint32_t color);
+void		put_pixel(mlx_image_t *image, uint32_t x,
+				uint32_t y, uint32_t color);
+uint32_t	get_texture_pixel(mlx_image_t *tex, int x, int y);
+void		init_ray(t_raycasting *ray, t_data *data);
+void		set_delta(t_raycasting *ray);
+void		set_sideDist(t_raycasting *ray);
 
-static uint32_t get_texture_pixel(mlx_image_t *tex, int x, int y)
+void	render_vertical_line(int x, t_raycasting *ray, t_data *data)
 {
-  int index;
+	mlx_image_t	*tex;
+	double		wallX;
+	int			texX;
+	int			texY;
+	double		step;
+	double		texPos;
+	int			y;
+	uint32_t	color;
 
-  if (x < 0 || x >= (int)tex->width || y < 0 || y >= (int)tex->height)
-      return (0x000000FF);
-  index = (y * tex->width + x) * 4;
+	if (ray->mapX < 0 || ray->mapX >= data->map.width
+		|| ray->mapY < 0 || ray->mapY >= data->map.height)
+		return ;
 
-  return (tex->pixels[index] << 24) |
-          (tex->pixels[index + 1] << 16) |
-          (tex->pixels[index + 2] << 8) |
-          (tex->pixels[index + 3]);
-}
+	if (ray->side == 0)
+	{
+		if (ray->rayDirX > 0)
+			tex = data->img[WE];
+		else
+			tex = data->img[EA];
+	}
+	else
+	{
+		if (ray->rayDirY > 0)
+			tex = data->img[NO];
+		else
+			tex = data->img[SO];
+	}
 
-void render_vertical_line(int x, t_raycasting *ray, t_data *data)
-{
-    mlx_image_t *tex;
-    double      wallX;
-    int         texX;
-    int         texY;
-    double      step;
-    double      texPos;
-    int         y;
-    uint32_t    color;
+	if (ray->side == 0)
+		wallX = ray->pos_y + ray->perpWallDist * ray->rayDirY;
+	else
+		wallX = ray->pos_x + ray->perpWallDist * ray->rayDirX;
 
-    if (ray->mapX < 0 || ray->mapX >= data->map.width ||
-        ray->mapY < 0 || ray->mapY >= data->map.height)
-        return;
+	wallX -= floor(wallX);
 
-    if (ray->side == 0)
-    {
-      if (ray->rayDirX > 0)
-        tex = data->img[WE];
-      else
-        tex = data->img[EA];
-    }
-    else
-    {
-      if (ray->rayDirY > 0)
-        tex = data->img[NO];
-      else
-        tex = data->img[SO];
-    }
+	texX = (int)(wallX * (double)tex->width);
 
-    if (ray->side == 0)
-        wallX = ray->pos_y + ray->perpWallDist * ray->rayDirY;
-    else
-        wallX = ray->pos_x + ray->perpWallDist * ray->rayDirX;
+	if (ray->side == 0 && ray->rayDirX > 0)
+		texX = tex->width - texX - 1;
+	if (ray->side == 1 && ray->rayDirY < 0)
+		texX = tex->width - texX - 1;
 
-    wallX -= floor(wallX);
+	step = 1.0 * tex->height / ray->lineHeight;
 
-    texX = (int)(wallX * (double)tex->width);
+	texPos = (ray->drawStart - WIN_HEIGHT / 2 + ray->lineHeight / 2) * step;
 
-    if (ray->side == 0 && ray->rayDirX > 0)
-        texX = tex->width - texX - 1;
-    if (ray->side == 1 && ray->rayDirY < 0)
-        texX = tex->width - texX - 1;
+	y = ray->drawStart;
+	if (x < 0 || x >= WIN_WIDTH)
+		return ;
 
-    step = 1.0 * tex->height / ray->lineHeight;
+	while (y < ray->drawEnd)
+	{
+		texY = (int)texPos & (tex->height - 1);
+		texPos += step;
 
-    texPos = (ray->drawStart - WIN_HEIGHT / 2 + ray->lineHeight / 2) * step;
-
-    y = ray->drawStart;
-    if (x < 0 || x >= WIN_WIDTH) return;
-
-    while (y < ray->drawEnd)
-    {
-        texY = (int)texPos & (tex->height - 1);
-        texPos += step;
-
-        color = get_texture_pixel(tex, texX, texY);
+		color = get_texture_pixel(tex, texX, texY);
 
 		if (ray->side == 1)
 			color = TINT_COLOR(color, TINT_FACTOR);
 
-        if (y >= 0 && y < WIN_HEIGHT)
-            mlx_put_pixel(data->screen, x, y, color);
-        y++;
-    }
+		if (y >= 0 && y < WIN_HEIGHT)
+			mlx_put_pixel(data->screen, x, y, color);
+		y++;
+	}
 }
 
-void init_ray(t_raycasting *ray, t_data *data)
+void	find_wall(t_raycasting *ray, t_data *data)
 {
-  ray->side = 0;
-  ray->pos_x = data->player[0];
-  ray->pos_y = data->player[1];
-  ray->plane_x = -data->dir_y * 0.66;
-  ray->plane_y = data->dir_x * 0.66;
-  ray->time = 0;
-  ray->oldTime = 0;
-  ray->mapY = (int)ray->pos_y;
-  ray->mapX = (int)ray->pos_x;
+	int	hit;
+
+	hit = 0;
+	while (hit == 0)
+	{
+		if (ray->sideDistX < ray->sideDistY)
+		{
+			ray->sideDistX += ray->deltaDistX;
+			ray->mapX += ray->stepX;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->sideDistY += ray->deltaDistY;
+			ray->mapY += ray->stepY;
+			ray->side = 1;
+		}
+		if (ray->mapX < 0 || ray->mapX >= data->map.width
+			|| ray->mapY < 0 || ray->mapY >= data->map.height)
+		{
+			hit = 1;
+			break ;
+		}
+		if (data->map.grid[ray->mapY][ray->mapX] > '0')
+			hit = 1;
+	}
+}
+//printf("mapX: %d\n mapY: %d\n", ray->mapX, ray->mapY);
+
+void	calculate_ray(t_raycasting *ray)
+{
+	if (ray->side == 0)
+		ray->perpWallDist = (ray->sideDistX - ray->deltaDistX);
+	else
+		ray->perpWallDist = (ray->sideDistY - ray->deltaDistY);
+	ray->lineHeight = (int)(WIN_HEIGHT / ray->perpWallDist);
+	ray->drawStart = -ray->lineHeight / 2 + WIN_HEIGHT / 2;
+	if (ray->drawStart < 0)
+		ray->drawStart = 0;
+	ray->drawEnd = ray->lineHeight / 2 + WIN_HEIGHT / 2;
+	if (ray->drawEnd >= WIN_HEIGHT)
+		ray->drawEnd = WIN_HEIGHT - 1;
 }
 
-void set_delta(t_raycasting *ray)
+void	raycasting(t_data *data)
 {
-  if (ray->rayDirX == 0)
-    ray->deltaDistX = 1e30;
-  else
-    ray->deltaDistX = fabs(1 / ray->rayDirX);
-  if (ray->rayDirY == 0)
-    ray->deltaDistY = 1e30;
-  else
-    ray->deltaDistY = fabs(1 / ray->rayDirY);
-}
+	t_raycasting	ray;
+	int				x;
 
-void set_sideDist(t_raycasting *ray)
-{
-  if (ray->rayDirX < 0)
-  {
-    ray->stepX = -1;
-    ray->sideDistX = (ray->pos_x - ray->mapX) * ray->deltaDistX;
-  }
-  else
-  {
-    ray->stepX = 1;
-    ray->sideDistX = (ray->mapX + 1.0 - ray->pos_x) * ray->deltaDistX;
-  }
-  if (ray->rayDirY < 0)
-  {
-    ray->stepY = -1;
-    ray->sideDistY = (ray->pos_y - ray->mapY) * ray->deltaDistY;
-  }
-  else
-  {
-    ray->stepY = 1;
-    ray->sideDistY = (ray->mapY + 1.0 - ray->pos_y) * ray->deltaDistY;
-  }
-}
-
-void find_wall(t_raycasting *ray, t_data *data)
-{
-  int hit;
-
-  hit = 0;
-  (void)data;
-  while (hit == 0)
-  {
-    if (ray->sideDistX < ray->sideDistY)
-    {
-      ray->sideDistX += ray->deltaDistX;
-      ray->mapX += ray->stepX;
-      ray->side = 0;
-    }
-    else
-    {
-      ray->sideDistY += ray->deltaDistY;
-      ray->mapY += ray->stepY;
-      ray->side = 1;
-    }
-    if (ray->mapX < 0 || ray->mapX >= data->map.width || ray->mapY < 0 || ray->mapY >= data->map.height)
-    {
-        //printf("mapX: %d\n mapY: %d\n", ray->mapX, ray->mapY);
-        hit = 1;
-        break;
-    }
-    if (data->map.grid[ray->mapY][ray->mapX] > '0')
-      hit = 1;
-  }
-}
-
-void calculate_ray(t_raycasting *ray)
-{
-  if (ray->side == 0)
-    ray->perpWallDist = (ray->sideDistX - ray->deltaDistX);
-  else
-    ray->perpWallDist = (ray->sideDistY - ray->deltaDistY);
-  ray->lineHeight = (int)(WIN_HEIGHT / ray->perpWallDist);
-  ray->drawStart = -ray->lineHeight / 2 + WIN_HEIGHT / 2;
-  if (ray->drawStart < 0)
-    ray->drawStart = 0;
-  ray->drawEnd = ray->lineHeight / 2 + WIN_HEIGHT / 2;
-  if (ray->drawEnd >= WIN_HEIGHT)
-    ray->drawEnd = WIN_HEIGHT - 1;
-}
-
-void raycasting(t_data *data)
-{
-  t_raycasting ray;
-  int           x;
-
-  x = 0;
-  init_ray(&ray, data);
-  fill_background(data);
-
-  while (x < (double)WIN_WIDTH)
-  {
-    ray.mapX = (int)ray.pos_x;
-    ray.mapY = (int)ray.pos_y;
-
-    ray.cameraX = 2 * x / (double)WIN_WIDTH - 1;
-    ray.rayDirX = data->dir_x + ray.plane_x * ray.cameraX;
-    ray.rayDirY = data->dir_y + ray.plane_y * ray.cameraX;
-
-    set_delta(&ray);
-    set_sideDist(&ray);
-    find_wall(&ray, data);
-    calculate_ray(&ray);
-    render_vertical_line(x, &ray, data);
-    x++;
-  }
+	x = 0;
+	init_ray(&ray, data);
+	fill_background(data);
+	while (x < (double)WIN_WIDTH)
+	{
+		ray.mapX = (int)ray.pos_x;
+		ray.mapY = (int)ray.pos_y;
+		ray.cameraX = 2 * x / (double)WIN_WIDTH - 1;
+		ray.rayDirX = data->dir_x + ray.plane_x * ray.cameraX;
+		ray.rayDirY = data->dir_y + ray.plane_y * ray.cameraX;
+		set_delta(&ray);
+		set_sideDist(&ray);
+		find_wall(&ray, data);
+		calculate_ray(&ray);
+		render_vertical_line(x, &ray, data);
+		x++;
+	}
 }
--- raycasting/utils.c
+++ raycasting/utils.c
@@ -1,12 +1,74 @@
 #include "../cub3d.h"
 
-void	put_pixel(mlx_image_t* image, uint32_t x, uint32_t y, uint32_t color)
+void	put_pixel(mlx_image_t *image, uint32_t x, uint32_t y, uint32_t color)
 {
-	uint8_t*	pixel;
+	uint8_t	*pixel;
 
 	pixel = &image->pixels[(y * image->width + x) * sizeof(int32_t)];
 	*(pixel++) = (uint8_t)(color >> 24);
 	*(pixel++) = (uint8_t)(color >> 16);
 	*(pixel++) = (uint8_t)(color >> 8);
 	*(pixel++) = (uint8_t)(color & 0xFF);
+}
+
+uint32_t	get_texture_pixel(mlx_image_t *tex, int x, int y)
+{
+	int	index;
+
+	if (x < 0 || x >= (int)tex->width || y < 0 || y >= (int)tex->height)
+		return (0x000000FF);
+	index = (y * tex->width + x) * 4;
+	return ((tex->pixels[index] << 24)
+		| (tex->pixels[index + 1] << 16)
+		| (tex->pixels[index + 2] << 8)
+		| (tex->pixels[index + 3]));
+}
+
+void	init_ray(t_raycasting *ray, t_data *data)
+{
+	ray->side = 0;
+	ray->pos_x = data->player[0];
+	ray->pos_y = data->player[1];
+	ray->plane_x = -data->dir_y * 0.66;
+	ray->plane_y = data->dir_x * 0.66;
+	ray->time = 0;
+	ray->oldTime = 0;
+	ray->mapY = (int)ray->pos_y;
+	ray->mapX = (int)ray->pos_x;
+}
+
+void	set_delta(t_raycasting *ray)
+{
+	if (ray->rayDirX == 0)
+		ray->deltaDistX = 1e30;
+	else
+		ray->deltaDistX = fabs(1 / ray->rayDirX);
+	if (ray->rayDirY == 0)
+		ray->deltaDistY = 1e30;
+	else
+		ray->deltaDistY = fabs(1 / ray->rayDirY);
+}
+
+void	set_sideDist(t_raycasting *ray)
+{
+	if (ray->rayDirX < 0)
+	{
+		ray->stepX = -1;
+		ray->sideDistX = (ray->pos_x - ray->mapX) * ray->deltaDistX;
+	}
+	else
+	{
+		ray->stepX = 1;
+		ray->sideDistX = (ray->mapX + 1.0 - ray->pos_x) * ray->deltaDistX;
+	}
+	if (ray->rayDirY < 0)
+	{
+		ray->stepY = -1;
+		ray->sideDistY = (ray->pos_y - ray->mapY) * ray->deltaDistY;
+	}
+	else
+	{
+		ray->stepY = 1;
+		ray->sideDistY = (ray->mapY + 1.0 - ray->pos_y) * ray->deltaDistY;
+	}
 }
