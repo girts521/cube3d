f2dbbe3ddbcd388ed293b5e1eb94aeb246ab92e1
Alex <email@test.com>
2026-01-15 21:12:32 +0100

    some norm (parser for now)

--- cub3d.h
+++ cub3d.h
@@ -1,81 +1,89 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include ".MLX42/include/MLX42/MLX42.h"
-#include "libft/libft.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+# include ".MLX42/include/MLX42/MLX42.h"
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
 
-#define WIN_WIDTH 1280
-#define WIN_HEIGHT 740
+# define WIN_WIDTH 1280
+# define WIN_HEIGHT 740
 
-#define N_TEXTURES 4
+# define N_TEXTURES 4
 
-#define TINT_FACTOR 0.8f
+# define TINT_FACTOR 0.8f
 
-#define TINT_COLOR(c, f)( \
+# define TINT_COLOR(c, f)( \
 	((int)(((c >> 24) & 0xFF) * (f)) << 24) | \
 	((int)(((c >> 16) & 0xFF) * (f)) << 16) | \
 	((int)(((c >>  8) & 0xFF) * (f)) <<  8) | \
 	((int)((c & 0xFF) * (f))) \
 )
 
-typedef enum texture {NO, SO, WE, EA} t_texture;
+typedef enum texture
+{
+	NO,
+	SO,
+	WE,
+	EA
+}	t_texture;
 
 typedef struct s_raycasting
 {
-  double  pos_x;
-  double  pos_y;
-  double  plane_x;
-  double  plane_y;
-  double  time;
-  double  oldTime;
-  double  cameraX;
-  double  rayDirX;
-  double  rayDirY;
-  int     mapX;
-  int     mapY;
-  double  sideDistX;
-  double  sideDistY;
-  double  deltaDistX;
-  double  deltaDistY;
-  int     stepX;
-  int     stepY;
-  int     side;
-  double  perpWallDist;
-  int     lineHeight;
-  int     drawStart;
-  int     drawEnd;
-  int     color;
-  char    wall_type;
-}              t_raycasting;
+	double	pos_x;
+	double	pos_y;
+	double	plane_x;
+	double	plane_y;
+	double	time;
+	double	oldTime;
+	double	cameraX;
+	double	rayDirX;
+	double	rayDirY;
+	int		mapX;
+	int		mapY;
+	double	sideDistX;
+	double	sideDistY;
+	double	deltaDistX;
+	double	deltaDistY;
+	int		stepX;
+	int		stepY;
+	int		side;
+	double	perpWallDist;
+	int		lineHeight;
+	int		drawStart;
+	int		drawEnd;
+	int		color;
+	char	wall_type;
+}	t_raycasting;
 
-typedef struct s_map {
-  char **grid;
-  int height;
-  int width;
-} t_map;
+typedef struct s_map
+{
+	char	**grid;
+	int		height;
+	int		width;
+}	t_map;
 
-typedef struct s_data {
-  mlx_t *mlx;
-  mlx_image_t *img[N_TEXTURES];
-  mlx_image_t *screen;
-  int floor[3];
-  int ceiling[3];
-  t_map map;
-  double player[3];
-  double dir_x;
-  double dir_y;
-  double plane_x;
-  double plane_y;
+typedef struct s_data
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img[N_TEXTURES];
+	mlx_image_t	*screen;
+	int			floor[3];
+	int			ceiling[3];
+	t_map		map;
+	double		player[3];
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
 
-} t_data;
+}	t_data;
 
-void parse_input(t_data *data, char *argv[]);
-int clean(t_data *data, char *err_mess, int status, int fd);
-void  fill_background(t_data *data);
-void raycasting(t_data *data);
+void	parse_input(t_data *data, char *argv[]);
+int		clean(t_data *data, char *err_mess, int status, int fd);
+void	fill_background(t_data *data);
+void	raycasting(t_data *data);
 
 #endif
--- parser/parse_map.c
+++ parser/parse_map.c
@@ -1,6 +1,6 @@
 #include "../cub3d.h"
 
-int check_trailing_lines(int fd, t_data *data);
+int	check_trailing_lines(int fd, t_data *data);
 
 static int	check_elements_presence(t_data *data)
 {
@@ -20,7 +20,7 @@ static int	set_width(char *line, t_data *data)
 	int	i;
 
 	i = 0;
-	while(line[i])
+	while (line[i])
 	{
 		if (!ft_ischars(line[i], " 01NSEW"))
 		{
@@ -47,8 +47,8 @@ static void	set_height(char *line, t_data *data, int fd)
 		line = get_next_line(fd, &malloc_failure, 0);
 		if (malloc_failure)
 			clean(data, "failure inside gnl\n", 1, fd);
-		if (!line || *line == '\0') // check \n basically
-			break;
+		if (!line || *line == '\0')
+			break ;
 		if (set_width(line, data))
 			clean(data, "Invalid map symbol\n", 1, fd);
 		free(line);
@@ -56,13 +56,14 @@ static void	set_height(char *line, t_data *data, int fd)
 	}
 	data->map.height = i;
 }
+// if (!line || *line == '\0') // check \n basically
 
 static void	map_dimensions(char *line, t_data *data, int fd)
 {
 	if (set_width(line, data))
 		clean(data, "Invalid map symbol\n", 1, fd);
 	set_height(line, data, fd);
-	if(check_trailing_lines(fd, data))
+	if (check_trailing_lines(fd, data))
 	{
 		free(line);
 		clean(data, "Garbage after map\n", 1, fd);
@@ -77,12 +78,5 @@ void	parse_map(char *line, t_data *data, int fd)
 		clean(data, "Invalid map symbol\n", 1, fd);
 	}
 	map_dimensions(line, data, fd);
-
-
-	//printf("\nwidth: %d, height: %d\n", data->map.width, data->map.height);
-
-
-	// restart with read(fd_len) to parse map properly
-
 	free(line);
 }
